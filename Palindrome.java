3615088516ec9a986c095598c9a4ee6f3ae8fd1a
katara10 <mari.popova.2006k@bk.ru>
2025-09-21 19:17:45 +0300

    Add files via upload
    Лабораторная работа №1

---
+++ Palindrome.java
@@ -0,0 +1,24 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            if (isPalindrome(s)) {
+                System.out.println(s + " palin");
+            } else {
+                System.out.println(s + " not palin");
+            }
+        }
+    }
+
+    public static String reverseString (String s) {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed += s.charAt(i);
+    }
+        return reversed;
+    }
+    public static boolean isPalindrome (String s) {
+        String reversed = reverseString(s);
+        return s.equals(reversed);
+    }
+}
---
+++ Primes.java
@@ -0,0 +1,17 @@
+public class Primes {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static boolean isPrime(int n) {
+        for (int x = 2; x <= Math.sqrt(n); x++) {
+            if (n % x == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
